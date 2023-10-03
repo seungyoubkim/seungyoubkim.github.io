ea8a295b15581b394d859707b85203f3e9e421ad
seungyoubkim <shas15@kakao.com>
2023-10-03 22:19:21 +0900

    update giscus

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,7 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import Utterances from '../components/Utterances'
+import Giscus from "../components/Giscus";
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
@@ -61,7 +61,7 @@ const BlogPostTemplate = ({ data, location }) => {
           </li>
         </ul>
       </nav>
-      <Utterances />
+      <Giscus />
     </Layout>
   )
 }
